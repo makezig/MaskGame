530e536895447d43ba045551ef4cae6ae7771ad3
makezig <markus.werty@gmail.com>
2026-01-31 19:10:59 +0200

    Bottle brake

--- mask-game/Assets/Bottle/Explode.gd
+++ mask-game/Assets/Bottle/Explode.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 
 @export var INTENSITY:float = 8.0;
+var centre = null;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
@@ -9,8 +10,10 @@ func _ready() -> void:
 	for bottlePieces:RigidBody3D in self.get_children():
 		
 		var dir = (bottlePieces.global_position - global_position).normalized()
-		bottlePieces.apply_impulse(dir * INTENSITY);
+		bottlePieces.apply_impulse(dir   * INTENSITY);
 		
 	await get_tree().create_timer(5.0).timeout
 	queue_free()
-	
+
+func set_centre(origin: Vector3):
+	centre = origin;
---
+++ mask-game/Assets/Bottle/Model_Shifter.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+@export var broken_model:PackedScene;
+@export var simple_model:Node3D;
+
+func ready():
+	print_tree_pretty()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		
+		var bottle_transform: Transform3D = simple_model.global_transform
+		print_debug(bottle_transform)
+		
+		#Instantiate broken model to bottle's position
+		var broken_model_inst:Node3D = broken_model.instantiate();
+		broken_model_inst.set_centre(bottle_transform.origin)	
+		
+		#get_parent().add_child(broken_model_inst);
+		get_tree().current_scene.add_child(broken_model_inst)
+		broken_model_inst.global_transform = bottle_transform
+		
+		print("Bottle:", global_position)
+		print("Broken:", broken_model_inst.global_position)
+		
+		print("Bottle parent:", get_parent().name)
+		
+		self.queue_free(); #Destroy model
+		
